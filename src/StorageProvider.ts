383ba4ac4b85c15695b67577f93ea8dbcc748b11
Benjamin Leber <grisu118@grisu118.ch>
2018-03-03 14:32:14 +0100

    c Add JSDoc

--- src/StorageProvider.ts
+++ src/StorageProvider.ts
@@ -2,6 +2,11 @@ import { UrlQueryHelper } from "./UrlQueryHelper";
 
 export class StorageProvider {
 
+  /**
+   * Returns a {@link Storage} which will use the localstorage as storage backend.
+   * @param {string} prefix A prefix for all keys managed over this storage instance.
+   * @returns {Storage} The storage.
+   */
   public static localStorage(prefix?: string): Storage {
     return new StorageImpl((key, value) => localStorage.setItem(key, value),
       (key) => localStorage.getItem(key) || undefined,
@@ -9,6 +14,11 @@ export class StorageProvider {
       prefix);
   }
 
+  /**
+   * Returns a {@link Storage} which will use the sessionstorage as storage backend.
+   * @param {string} prefix A prefix for all keys managed over this storage instance.
+   * @returns {Storage} The storage.
+   */
   public static sessionStorage(prefix?: string): Storage {
     return new StorageImpl((key, value) => sessionStorage.setItem(key, value),
       (key) => sessionStorage.getItem(key) || undefined,
@@ -16,6 +26,11 @@ export class StorageProvider {
       prefix);
   }
 
+  /**
+   * Returns a {@link Storage} which will use url get parameters as storage.
+   * @param {string} prefix A prefix for all keys managed over this storage instance.
+   * @returns {Storage} The storage.
+   */
   public static urlStorage(prefix?: string): Storage {
     return new StorageImpl((key, value) => {
         const currentParams = UrlQueryHelper.getQueryValues();
@@ -128,19 +143,60 @@ class StorageImpl implements Storage {
 }
 
 export interface Storage {
+  /**
+   * Sets the given value with the given key into the storage.
+   * Objects are serialized with <code>JSON.stringify</code>.
+   * @param {string} key the key where to save the value
+   * @param value the value to store.
+   */
   set(key: string, value: any);
 
+  /**
+   * Returns the value for the given key as string.
+   * This method will always return a value if the key exists, because all values are saved as string!
+   * @param {string} key the key of the value to access.
+   * @returns {string | undefined} the value if existing or undefined otherwise.
+   */
   getAsString(key: string): string | undefined;
 
+  /**
+   * Returns the value for the given key as number. Tries to parse the value at the given key as number.
+   * @param {string} key the key of the value to access.
+   * @returns {number | undefined} the value as number if existing or readable as number. Otherwise undefined!
+   */
   getAsNumber(key: string): number | undefined;
 
+  /**
+   * Returns the value for the given key as boolean. Tries to parse the value at the given key as boolean.
+   * @param {string} key the key of the value to access.
+   * @returns {boolean | undefined} the value as boolean if existing or readable as boolean. Otherwise undefined!
+   */
   getAsBoolean(key: string): boolean | undefined;
 
+  /**
+   * Returns the value for the given key as object. Uses JSON.parse for parsing.
+   * @param {string} key the key of the value to access.
+   * @returns {object | undefined} the object if JSON.parse is able to read it, otherwise undefined.
+   */
   getAsObject(key: string): object | undefined;
 
+  /**
+   * Uses same logic as {@link getAsObject}. Use {@link getAsObject} for other arrays.
+   * @param {string} key the key of the value to access.
+   * @returns {string[] | undefined} the string[] if the stored value is readable as string[], otherwise undefined.
+   */
   getAsArray(key: string): string[] | undefined;
 
+  /**
+   * Similar to {@link getAsString}
+   * @param {string} key the key of the value to access.
+   * @returns {string | undefined} the value if existing or undefined otherwise.
+   */
   get(key: string): string | undefined;
 
+  /**
+   * Deletes the given key from the storage.
+   * @param {string} key the key of the value to delete.
+   */
   del(key: string);
 }
